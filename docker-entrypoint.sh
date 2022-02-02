330e44bc99af0c2b397d32295067dbb437f35c30
Alejandro Dominguez <alejandroomin@gmail.com>
2022-02-02 12:48:11 +0100

    Subida a repositorio

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i 's/$this->user="root";/$this->user="'${DATABASE_USER}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->pass="";/$this->pass="'${DATABASE_PASSWORD}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->host="localhost";/$this->host="'${DATABASE_HOST}'";/g' /var/www/html/core/controller/Database.php
+
+
+mysql -h $DATABASE_HOST -u $DATABASE_USER -p$DATABASE_PASSWORD bookmedik < /var/www/html/schema.sql
+
+apache2ctl -D FOREGROUND
